5dec307832ab1eeec8ad241f1c20f390b2a0e7be
Vladimír Svoboda <vlada.svoboda@volny.cz>
2020-05-28 13:56:39 +0200

    Porovnání podle original ID, skrytí new a missing

--- CompareReqGUI/mainwindow.cpp
+++ CompareReqGUI/mainwindow.cpp
@@ -145,7 +145,7 @@ MainWindow::MainWindow(QStringList arguments, QWidget *parent) :
       }
 #endif
       asUserAndHostName += "@" + QHostInfo::localHostName();
-      qDebug() << asUserAndHostName;
+      //qDebug() << asUserAndHostName;
 
       boBatchProcessing = false;
 
@@ -730,6 +730,8 @@ void MainWindow::on_btnCompare_clicked()
     lstNewReqIDs.clear();
     QStringList lstOldReqIDs;
     lstOldReqIDs.clear();
+
+    //START COMPARING - FIRST LOOK TO THE OLD FILES...
     //read all rows in old files
     for (int oldRow = kn_FistDataRow; oldRow <= oldLastRow; ++oldRow)
     {
@@ -740,30 +742,106 @@ void MainWindow::on_btnCompare_clicked()
            asOldReqID = asOldReqID.remove(0, asOldReqID.indexOf("TSAnS"));
        }
 
+
+       //USE GUID
        if(ui->cb_UseGUID->isChecked())
        {
-          asOldReqID = getCellValue(oldRow, mapOldHeaders["GUID"], oldReqDoc, true);
+           int iMappedColumn = mapOldHeaders["GUID"];
+           if(iMappedColumn > 0)
+           {
+             asOldReqID = getCellValue(oldRow, iMappedColumn, oldReqDoc, true);
+           }
+           else
+           {
+               if (!boBatchProcessing)
+               {
+                   QMessageBox::information(this, "Error", "No GUID in old", QMessageBox::Ok);
+               }
+               else
+               {
+                  qWarning() << "Error " <<  "No GUID in old";
+                  //TODO exit code
+               }
+               return;
+            }
        }
+
+
+
+
 
        lstOldReqIDs << asOldReqID;
        int iOldReqID      = -1; //TODO
        Q_UNUSED(iOldReqID)
 
+       //FOR CURRENT ITEM IN THE OLF FILE CHECK EVERY ITEM IN NEW FILE
        //and compare with every row in new file..
        bool bo_idFound = false;
        for(int newRow = kn_FistDataRow; newRow <= newLastRow; ++newRow)
        {
-         QString asNewReqID = getCellValue(newRow, kn_ReqIDCol, newReqDoc, true);
+         QString asNewReqIDForCompare = getCellValue(newRow, kn_ReqIDCol, newReqDoc, true);
          //ignore project prefix
          if(ui->cb_IgnoreProject->isChecked())
          {
-              asNewReqID = asNewReqID.remove(0, asNewReqID.indexOf("TSAnS"));
+              asNewReqIDForCompare = asNewReqIDForCompare.remove(0, asNewReqIDForCompare.indexOf("TSAnS"));
          }
+         //real new ID
+         QString asNewReqID = asNewReqIDForCompare;
 
+         //USE GUID
          if(ui->cb_UseGUID->isChecked())
          {
-             asNewReqID = getCellValue(newRow, mapNewHeaders["GUID"], newReqDoc, true);
+             int iMappedColumn = mapNewHeaders["GUID"];
+             if(iMappedColumn > 0)
+             {
+               asNewReqIDForCompare = getCellValue(newRow, iMappedColumn, newReqDoc, true);
+             }
+             else
+             {
+                 if (!boBatchProcessing)
+                 {
+                     QMessageBox::information(this, "Error", "No GUID in new", QMessageBox::Ok);
+                 }
+                 else
+                 {
+                    qWarning() << "Error " <<  "No GUID in new";
+                    //TODO exit code
+                 }
+                 return;
+
+             }
+
          }
+
+
+         //USE ID Original (DXL)
+         if(ui->cb_UseOriginalID->isChecked())
+         {
+             int iMappedColumn = mapNewHeaders["ID Original (DXL)"];
+             if(iMappedColumn > 0)
+             {
+               asNewReqIDForCompare = getCellValue(newRow, iMappedColumn, newReqDoc, true);
+             }
+             else
+             {
+                 if (!boBatchProcessing)
+                 {
+                     QMessageBox::information(this, "Error", "No ID Original (DXL) in new", QMessageBox::Ok);
+                 }
+                 else
+                 {
+                    qWarning() << "Error " <<  "No ID Original (DXL) in new";
+                    //TODO exit code
+                 }
+                 return;
+              }
+
+         }
+
+
+
+
+
 
 
          int iNewReqID    = -1; //TODO
@@ -771,12 +849,14 @@ void MainWindow::on_btnCompare_clicked()
          //add ids only once
          if(oldRow == kn_FistDataRow)
          {
-           lstNewReqIDs << asNewReqID;
+           lstNewReqIDs << asNewReqIDForCompare;
          }
 
          //if ID requirments are same, check every column which are in both files
-         if(asOldReqID == asNewReqID)
+         //qDebug() << "1" << asOldReqID << asNewReqID << asNewReqIDForCompare;
+         if(asOldReqID == asNewReqIDForCompare)
          {
+             qDebug() << "2" << asOldReqID << asNewReqID << asNewReqIDForCompare;
              bo_idFound = true;
              bool boSameReq = false;
              //and now check every column which is in both files.. //TODO maybe better in at least one!!
@@ -825,15 +905,15 @@ void MainWindow::on_btnCompare_clicked()
                   )
                 //different
                 {
-                   qDebug() << "different:";
-                   qDebug() << asOldReqID << asHeader <<  asOldColValue << asNewColValue;
+                   //qDebug() << "different:";
+                   //qDebug() << asOldReqID << asHeader <<  asOldColValue << asNewColValue;
 
                    ui->tableWidget_Changes->insertRow(ui->tableWidget_Changes->rowCount());
-                   ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_requirement, new QTableWidgetItem(asOldReqID));
+                   ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_requirement, new QTableWidgetItem(asNewReqID));
                    QString asShort = "";
                    if (!ui->cb_UseGUID->isChecked())
                    {
-                      asShort = asOldReqID.section("_", -1);
+                      asShort = asNewReqID.section("_", -1);
                    }
                    else
                    {
@@ -849,7 +929,7 @@ void MainWindow::on_btnCompare_clicked()
                        ui->tableWidget_Changes->item(ui->tableWidget_Changes->rowCount()-1, col_infotable_req_short)->setBackground(QBrush(QColor(Qt::lightGray)));
 
                        //denote previous line also - should belog to the same req id
-                       if (ui->tableWidget_Changes->rowCount() > 2)
+                       if (ui->tableWidget_Changes->rowCount() > 1)
                        {
                            QTableWidgetItem * tmpTableWidgetItem = nullptr;
                            tmpTableWidgetItem = ui->tableWidget_Changes->item((ui->tableWidget_Changes->rowCount()-1)-1, col_infotable_requirement);
@@ -866,7 +946,11 @@ void MainWindow::on_btnCompare_clicked()
                    ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_par_name,  new QTableWidgetItem(asHeader));
                    ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_old_value, new QTableWidgetItem(asOldColValue));
                    ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_new_value, new QTableWidgetItem(asNewColValue));
-                   QString asOrigID = getCellValue(oldRow, mapOldHeaders[knOrigIDText], oldReqDoc, true);
+                   QString asOrigID = "";
+                   if(ui->cb_FindSimilarFromNew->isChecked())
+                     asOrigID = getCellValue(oldRow, mapOldHeaders[knOrigIDText], oldReqDoc, true);
+                   if(ui->cb_UseOriginalID->isChecked())
+                     asOrigID = asOldReqID;
                    ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_origID, new QTableWidgetItem(asOrigID));
 
                    //if compare without demand for ignorig white space and case, compare once more ignored and if same (e.g. difference is only in spaces and case
@@ -904,40 +988,46 @@ void MainWindow::on_btnCompare_clicked()
        }//for(int newRow = kn_FistDataRow; newRow <= newLastRow; ++newRow)
 
        //not found in new - only in old req
-       if(!bo_idFound)
+       if(!ui->cb_HideMissing->isChecked())
        {
-           //qDebug() << "not in new file:";
-           //qDebug() << asOldReqID;
-           ui->tableWidget_Changes->insertRow(ui->tableWidget_Changes->rowCount());
-           ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_requirement, new QTableWidgetItem(asOldReqID));
-           ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_req_short, new QTableWidgetItem(getCellValue(oldRow, kn_ReqIDCol, oldReqDoc,  true).section("_", -1)));
-           ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_status,      new QTableWidgetItem("Missing"));
-           ui->tableWidget_Changes->item(ui->tableWidget_Changes->rowCount()-1, col_infotable_status)->setBackground(QBrush(QColor(Qt::red)));
+           if(!bo_idFound)
+           {
+               //qDebug() << "not in new file:";
+               //qDebug() << asOldReqID;
+               ui->tableWidget_Changes->insertRow(ui->tableWidget_Changes->rowCount());
+               ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_requirement, new QTableWidgetItem(asOldReqID));
+               ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_req_short, new QTableWidgetItem(getCellValue(oldRow, kn_ReqIDCol, oldReqDoc,  true).section("_", -1)));
+               ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_status,      new QTableWidgetItem("Missing"));
+               ui->tableWidget_Changes->item(ui->tableWidget_Changes->rowCount()-1, col_infotable_status)->setBackground(QBrush(QColor(Qt::red)));
+           }
        }
-
 
     }//for (int oldRow = kn_FistDataRow; oldRow <= oldLastRow; ++oldRow)
 
+    //TO DO CHECK
     QStringList lstNewReqIDsOnly =  (QSet<QString>(lstNewReqIDs.begin(), lstNewReqIDs.end())
                                      .subtract(QSet<QString>(lstOldReqIDs.begin(), lstOldReqIDs.end()))).values();
 
     lstNewReqIDsOnly.sort();
 
-    //Only in new file (new req)
-    foreach (QString asNewIDOnly, lstNewReqIDsOnly)
+    if(!ui->cb_HideNew->isChecked())
     {
-       //qDebug() << "New ID only "  << asNewIDOnly;
-       ui->tableWidget_Changes->insertRow(ui->tableWidget_Changes->rowCount());
-       ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_requirement, new QTableWidgetItem(asNewIDOnly));
-       if (!ui->cb_UseGUID->isChecked())
-         ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_req_short, new QTableWidgetItem(asNewIDOnly.section("_", -1)));
-       else
-         ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_req_short, new QTableWidgetItem(""));  //TODO
-       ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_status,      new QTableWidgetItem("New"));
-       ui->tableWidget_Changes->item(ui->tableWidget_Changes->rowCount()-1, col_infotable_status)->setBackground(QBrush(QColor(Qt::green)));
 
+        //Only in new file (new req)
+        foreach (QString asNewIDOnly, lstNewReqIDsOnly)
+        {
+           //qDebug() << "New ID only "  << asNewIDOnly;
+           ui->tableWidget_Changes->insertRow(ui->tableWidget_Changes->rowCount());
+           ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_requirement, new QTableWidgetItem(asNewIDOnly));
+           if (!ui->cb_UseGUID->isChecked())
+             ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_req_short, new QTableWidgetItem(asNewIDOnly.section("_", -1)));
+           else
+             ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_req_short, new QTableWidgetItem(""));  //TODO
+           ui->tableWidget_Changes->setItem(ui->tableWidget_Changes->rowCount()-1, col_infotable_status,      new QTableWidgetItem("New"));
+           ui->tableWidget_Changes->item(ui->tableWidget_Changes->rowCount()-1, col_infotable_status)->setBackground(QBrush(QColor(Qt::green)));
+
+        }
     }
-
 
     //adjust table width
     ui->tableWidget_Changes->resizeColumnsToContents();
@@ -978,6 +1068,10 @@ void MainWindow::on_btnCompare_clicked()
     if(ui->cb_FindSimilarFromNew->isChecked())  asCompareCondition += ":Find Similar:";
     if(ui->cb_UseGUID->isChecked())             asCompareCondition += ":Use GUID:";
     if(ui->cb_IgnoreID->isChecked())            asCompareCondition += ":Ignore ID:";
+    if(ui->cb_HideMissing->isChecked())         asCompareCondition += ":Hide Missing:";
+    if(ui->cb_HideNew->isChecked())             asCompareCondition += ":Hide New:";
+    if(ui->cb_UseOriginalID->isChecked())         asCompareCondition += ":Use Origin ID:";
+    if(ui->cb_Reserve1->isChecked())             asCompareCondition += ":Reserve 1:";
     if(boBatchProcessing)                       asCompareCondition += ":Batch Processing:";
 
 //Try to find if some req with differnt ID did not match in some parts
@@ -1042,12 +1136,12 @@ void MainWindow::on_btnCompare_clicked()
 //                                    asOldRequirement,
 //                                    asNewArchRemark,
 //                                    asOldArchRemark);
-                qDebug() << oldRow << newRow
-                         << asNewReqID
-                         << asOldReqID
-                         << asNewRequirement
-                         << asOldRequirement
-                         << asNewArchRemark;
+//                //qDebug() << oldRow << newRow
+//                         << asNewReqID
+//                         << asOldReqID
+//                         << asNewRequirement
+//                         << asOldRequirement
+//                         << asNewArchRemark;
               }//if
             }//for (int oasOldArchRemark);ldRow = kn_FistDataRow; oldRow <= oldLastRow; ++oldRow)
           }//if(asNewReqID == asNewIDOnly)
@@ -1096,7 +1190,7 @@ void MainWindow::on_btnCompare_clicked()
 
 void MainWindow::on_tableWidget_Changes_clicked(const QModelIndex &index)
 {
-    qDebug() << index.row() << index.column();
+    //qDebug() << index.row() << index.column();
 
 
     QString asOldText = "";
